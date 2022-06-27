4f49632a289a61969360a8f43b9f61348f9c767d
Quentin Diebold <quentus123@orange.fr>
2022-06-27 11:13:51 +0200

    changed class name

--- src/nodeHwProvider.ts
+++ src/nodeHwProvider.ts
@@ -2,7 +2,7 @@ import {HWProvider} from "./hwProvider";
 import Transport from "ledgerhq__hw-transport";
 import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
 
-export class NodeHwProvider extends HWProvider {
+export class NodeHWProvider extends HWProvider {
 
     async getTransport(): Promise<Transport> {
         return await TransportNodeHid.create();
